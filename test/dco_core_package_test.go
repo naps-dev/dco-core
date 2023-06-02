9dcf622e3ca2cbb7481c0ff4146a44d68aa980a2
Ben Mountjoy <benmountjoy@gmail.com>
2023-06-02 13:59:24 -0600

    st

--- test/dco_core_package_test.go
+++ test/dco_core_package_test.go
@@ -84,7 +84,7 @@ func TestZarfPackage(t *testing.T) {
 		Command: "zarf",
 		Args: []string{"package", "deploy", "../dco-core/zarf-package-dco-core-amd64.tar.zst", "--confirm",
 			"--components", "flux,bigbang,setup,kubevirt,cdi,metallb,metallb-config,dataplane-ek",
-			"--set", "METALLB_IP_ADDRESS_POOL=" + ipstart.String() + "-" + ipend.String(), "--log-level=debug"
+			"--set", "METALLB_IP_ADDRESS_POOL=" + ipstart.String() + "-" + ipend.String(),
 		},
 		Env: testEnv,
 	}
